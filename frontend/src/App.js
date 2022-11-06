29fccbadc6e95e68ba5e5ede8f2156902dd968bb
Joe Dravarol <joedravarol@gmail.com>
2022-11-06 01:43:14 +0000

    feat: Create static calendar UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
+import Calendar from './components/Calendar';
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Calendar />
   )
 }
 
