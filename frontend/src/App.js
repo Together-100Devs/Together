674efdac8fd76f3cdab7874037db571352b510f9
Caleb Cohen <calebcohen@hotmail.com>
2022-11-08 18:50:42 -0600

    redirect update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div>
       <Calendar />
       {/* Test Link for Discord Authorization. Link comes from discord developer tools, redirects back to localhost:3000 for now */}
-      <a href="https://discord.com/api/oauth2/authorize?client_id=1039303417199345684&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&scope=identify%20guilds">Login With Discord</a>
+      <a href="https://discord.com/api/oauth2/authorize?client_id=1039303417199345684&redirect_uri=https%3A%2F%2Ftogether.cyclic.app%2F&response_type=code&scope=identify%20email%20guilds%20guilds.join">Login With Discord</a>
     </div>
   )
 }
