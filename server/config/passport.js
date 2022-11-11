9eaccc8402f53d66ac729858dfd3359154f4bc1a
neoneoexe <albin.rushiti99@gmail.com>
2022-11-11 19:24:18 +0100

    new refactoring

---
+++ server/config/passport.js
@@ -0,0 +1,62 @@
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const User = require("../models/User");
+
+const DiscordStrategy = require('passport-discord').Strategy;
+// pulls discord username without email, and returns basic information about all the user's current guilds / servers. 
+const scopes = ['identify', 'guilds']
+
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      User.findOne({ email: email.toLowerCase() }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { msg: `Email ${email} not found.` });
+        }
+        if (!user.password) {
+          return done(null, false, {
+            msg:
+              "Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.",
+          });
+        }
+        user.comparePassword(password, (err, isMatch) => {
+          if (err) {
+            return done(err);
+          }
+          if (isMatch) {
+            return done(null, user);
+          }
+          return done(null, false, { msg: "Invalid email or password." });
+        });
+      });
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
+  });
+
+  //Discord authentication
+  passport.use(new DiscordStrategy({
+    //Get client ID and Secret from discord developer portal
+    clientID: process.env.DISCORD_CLIENT_ID,
+    clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    callbackURL: 'https://together.cyclic.app',
+    scope: scopes
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ discordId: profile.id }, function(err, user) {
+        return cb(err, user);
+    });
+  }));
+
+
+
+};
