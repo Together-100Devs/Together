3832370e3816449e27b51ae1dc2921801384b15c
Joe Dravarol <joedravarol@gmail.com>
2022-11-11 03:15:02 +0000

    refactor: Update action to relative path

--- frontend/src/components/LoginWithDiscord.js
+++ frontend/src/components/LoginWithDiscord.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoginWithDiscord = () => {
   return (
-    <form action="http://localhost:2121/auth/discord">
+    <form action="/auth/discord">
       <button type="submit">Login with Discord</button>
     </form>
   )
