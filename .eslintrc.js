d1a1ec0ea7d8d1628916c3925fa7f3b293bfc47d
Joe Dravarol <joedravarol@gmail.com>
2022-11-12 03:24:23 +0000

    feat: Add and configure Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    browser: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  rules: {
+    indent: [
+      "error",
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    "linebreak-style": ["error", "unix"],
+    eqeqeq: "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": [
+      "error",
+      {
+        before: true,
+        after: true,
+      },
+    ],
+    "no-console": 0,
+    "react/prop-types": 0,
+    "react/jsx-indent": [2, 2],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
