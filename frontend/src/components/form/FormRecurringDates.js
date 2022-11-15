63745acfab448b6a05ce1cb8e2af88bea20b814f
InstincDev <crene7722@gmail.com>
2022-11-15 01:06:15 -0600

    format recurring and confirm checkboxes

--- frontend/src/components/form/FormRecurringDates.js
+++ frontend/src/components/form/FormRecurringDates.js
@@ -15,30 +15,30 @@ export default function FormRecurringDates () {
             setUserData({ ...userData, [name]: value });
             }
       console.log(`${userData[[name]]}`)
-    }
-
-    const allChange = (e) => {
-      const { name, value, className, type,  checked } = e.target;
-        if (type=== "checkbox" && className === "day") {
-            setUserData({ ...userData, [name]: checked })
-        } else {
-            setUserData({ ...userData, [name]: value });
-            }
-      console.log(`${userData[[name]]}`)
-    }
-    
-    
-       
+    }      
     
   return (
     <div className ="w-full mx-2 flex-1">
       <div className="font-bold h-6 mt-3 text-grey-500 text-xs leading-8 uppercase">
      Recurring
       </div>
-      <div className="bg-white p-1 flex-col space-y-6 border border-gray-200 rounded">
-       <div className='flex space-x-4 items-center'>
+      <div className="bg-white py-4 p-1 my-2 flex border border-gray-200 rounded">
+    <div className="border-r pr-6">
+        <div className='flex space-x-4 items-center'>
         <input type="checkbox" 
-       onChange={allChange}
+       onChange={handleChange}
+       value='weekly'
+       name='weekly'
+       label="weekly"
+       checked={!!userData["weekly"]}
+       className="  mx-1 outline-non text-gray-800"
+       />
+        <p>Weekly</p>
+        </div>
+
+        <div className='flex space-x-4 items-center'>
+       <input type="checkbox" 
+       onChange={handleChange}
        value='monthly'
        name='monthly'
        label="monthly"
@@ -47,8 +47,21 @@ export default function FormRecurringDates () {
        />
        <p>Monthly</p>
        </div>
-       
-       <div className="mx-8 space-y-2">
+
+       <div className='flex space-x-4 items-center'>
+       <input type="checkbox" 
+       onChange={handleChange}
+       value='noRecurr'
+       name='noRecurr'
+       label="noRecurr"
+       checked={!!userData["noRecurr"]}
+       className="  mx-1 outline-non text-gray-800"
+       />
+       <p>No Recurring</p>
+       </div>
+
+       </div>
+       <div className=" mx-7 ">
         <div className='  flex space-x-4 items-center'>
          <input type="checkbox" 
        onChange={handleChange}
@@ -56,7 +69,7 @@ export default function FormRecurringDates () {
        name= 'monday'
        id='monday'
        checked={!!userData["monday"]}
-       className="day mx-1 outline-non text-gray-800"
+       className="mx-1 outline-non text-gray-800"
        />
         <p>Monday</p>
        </div>
@@ -67,7 +80,7 @@ export default function FormRecurringDates () {
        name='tuesday'
        id='tuesday'
        checked = {!!userData["tuesday"]}
-       className="day mx-1 outline-non text-gray-800"
+       className=" mx-1 outline-non text-gray-800"
        />
        <p>Tuesday</p>
        </div>
@@ -78,7 +91,7 @@ export default function FormRecurringDates () {
        name='wednesday'
        id='wednesday'
        checked={!!userData["wednesday"]}
-       className="day mx-1 outline-non text-gray-800"
+       className=" mx-1 outline-non text-gray-800"
        />
        <p>Wednesday</p>
        </div>
@@ -106,7 +119,7 @@ export default function FormRecurringDates () {
         <input type="checkbox" 
        onChange={handleChange}
        value='saturday'
-       name='Saturday'
+       name='saturday'
        checked={!!userData["saturday"]}
        className="mx-1 outline-non text-gray-800"
        />
@@ -123,7 +136,7 @@ export default function FormRecurringDates () {
        <p>Sunday</p>
        </div>
        </div>
-      </div>
+    </div>
     </div>
   )
 }
