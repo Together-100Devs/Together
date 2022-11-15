3484a9cf6d8c988a9331bbf75541f10330e7bc0b
InstincDev <crene7722@gmail.com>
2022-11-15 00:27:47 -0600

    add success pg to form

--- frontend/src/components/form/FormSuccess.js
+++ frontend/src/components/form/FormSuccess.js
@@ -1,75 +1,20 @@
 import {useContext} from 'react'
-import { FormMoverContext } from './FormMoverContext'
 
 
 export default function FormSuccess () {
-  const {userData, setUserData} = useContext(FormMoverContext)
-  
-  const handleChange = (e) => {
-    const {name, value} = e.target
-    setUserData({...userData, [name] : value})
-  }
- 
-  return (
-    <div className="flex flex-col">
-    <div className ="w-full mx-2 flex-1">
-      <div className="font-bold h-6 mt-3 text-grey-500 text-xs leading-8 uppercase">
-      Title
+  return(
+  <div className="container md:mt-10">
+    <div className="flex flex-col items-center">
+      <div className="pb-5">
+        Congratulations!
       </div>
-      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
-        <input type="text"
-        onChange={handleChange}
-        value={userData['title'] || ''}
-        name='title'
-        placeholder = "Title"
-        className="p-1 px-2 appearance-none outline-non w-full text-gray-800"
-         />
+      <div className="text-teal-700 text-lg font-semibold uppercase">
+        Your Event has been added.
       </div>
+      <a href="/" className="mt-10">
+        <button className="h-10 px-5 font-semibold text-green-700 transition-colors duration-150 border border-gray-300 rounded-lg focus:shadow-outline hover:bg-green-500 hover:text-green-100">New Event</button>
+      </a>
     </div>
-
-    <div className ="w-full mx-2 flex-1">
-      <div className="font-bold h-6 mt-3 text-grey-500 text-xs leading-8 uppercase">
-      Description
-      </div>
-      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
-        <input type="text"
-        onChange={handleChange}
-        value={userData['description'] || ''}
-        name='description'
-        placeholder = "Description"
-        className="p-1 px-2 appearance-none outline-non w-full text-gray-800"
-         />
-      </div>
-    </div>
-
-    <div className ="w-full mx-2 flex-1">
-      <div className="font-bold h-6 mt-3 text-grey-500 text-xs leading-8 uppercase">
-     Date
-      </div>
-      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
-        <input type="date"
-        onChange={handleChange}
-        value={userData['date'] || ''}
-        name='date'
-        placeholder = "Date"
-        className="p-1 px-2 appearance-none outline-non w-full text-gray-800"
-         />
-      </div>
-    </div>
-    <div className ="w-full mx-2 flex-1">
-      <div className="font-bold h-6 mt-3 text-grey-500 text-xs leading-8 uppercase">
-     Discord Name
-      </div>
-      <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
-        <input type="text"
-        onChange={handleChange}
-        value={userData['discordName'] || ''}
-        name='discordName'
-        placeholder = "Discord Name"
-        className="p-1 px-2 appearance-none outline-non w-full text-gray-800"
-         />
-      </div>
-    </div>
-    </div>
+  </div>
   )
 }
