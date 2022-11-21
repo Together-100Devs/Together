b403746765e0d00c9baba05a2a7dcb846e443fc8
Caleb Cohen <CalebCohen@hotmail.com>
2022-11-21 13:54:37 -0600

    removal of home.js and removal of home references as it is a BuB remnant

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const homeController = require("../controllers/home");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 const passport = require("passport")
 
 //Main Routes - simplified for now
-router.get("/", homeController.getIndex);
-//router.get("/add_event", homeController.getAdd);
 
 //Discord Authentication Routes
 router.get('/auth/discord', passport.authenticate('discord'));
