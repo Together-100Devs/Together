8aa6b871a8f202ea2c48458c1d4d91531bc14600
Caleb Cohen <CalebCohen@hotmail.com>
2022-12-02 05:33:44 -0600

    renaming of base_url to OAUTH_REDIRECT_URL. Moved client dev dependencies to root folder

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const passport = require("passport");
 
 //Main Routes - simplified for now
-const baseUrl = process.env.BASE_URL || 'http://localhost:3000'
+const authRedirectUrl = process.env.OAUTH_REDIRECT_URL || 'http://localhost:3000'
 //Discord Authentication Routes
 router.get("/auth/discord", passport.authenticate("discord"));
 router.get(
   "/auth/discord/callback",
   passport.authenticate("discord", {
-    failureRedirect: baseUrl,
-    successRedirect: baseUrl,
+    failureRedirect: authRedirectUrl,
+    successRedirect: authRedirectUrl,
   })
 );
 
