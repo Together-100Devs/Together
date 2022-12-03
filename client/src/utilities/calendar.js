93de5d3db63987722b8f3005b573a90466510f88
Joe Dravarol <joedravarol@gmail.com>
2022-12-03 12:35:28 +0000

    feat: Populate events from database data
    - Added noDevBuild proxy file
    - Fix bug where calendar is not being display when there is no events.
    - Display local time from event

--- client/src/utilities/calendar.js
+++ client/src/utilities/calendar.js
@@ -1,13 +1,11 @@
-import getDay from 'date-fns/getDay'
 import parseISO from 'date-fns/parseISO'
-import getMonth from 'date-fns/getMonth'
 import format from 'date-fns/format';
 
 export const getMatchMonth = (monthToMatch, events) => {
   if (!events.length) return [];
 
   return events.filter(event => {
-    const isoDate = parseISO(event.date);
+    const isoDate = parseISO(event.startDate);
     const monthInString = format(isoDate, 'LLLL'); // December
     return monthToMatch === monthInString
   })
@@ -17,8 +15,13 @@ export const getEventsByDayNumber = (currentDay, allEvents) => {
   if (!allEvents.length) return [];
 
   return allEvents.filter(event => {
-    const isoDate = parseISO(event.date);
+    const isoDate = parseISO(event.startDate);
     const day = format(isoDate, 'd'); // '2'
     return currentDay == day
   })
-} 
+}
+
+export const formatToLocalTime = date => {
+  const isoDate = parseISO(date);
+  return format(isoDate, 'p')
+}
