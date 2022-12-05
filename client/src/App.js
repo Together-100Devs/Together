e4deed0b6dab800644dd294a956ef1bd58843b30
Matthew Bozin <mbozin94@gmail.com>
2022-12-05 12:22:48 -0800

    Add context to App.js in client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "features/calendar/Calendar";
 import LoginWithDiscord from "features/auth/LoginWithDiscord";
 import UserForm from "features/form/UserForm";
+import DataService from "services/dataService";
+import { Context } from "./contexts/Context"
 
 function App() {
-  const [page, setPage] = useState("landingPage");  
+  const [context, setContext] = useState({user: null, page: "landingPage"})
 
-  if (page === "landingPage") {
-    return (
-      <>
+  const setPage = (page) => {
+    context.page = page
+    setContext({...context})
+  }
+
+  useEffect(() => {
+    DataService.getCurrentUser().then(response => {
+      context.user = response.data;
+      setContext({...context})
+      console.log(context.user)
+    });
+  }, []);
+
+  return (
+    <Context.Provider value={[context, setContext]}>
+      {context.page === "landingPage" && <>
         <h1>Hello, landingPage goes here</h1>
         <button onClick={() => setPage("calendarPage")}>
           Navigate to Calendar
         </button>
-      </>
-    ) 
-  } else if (page === "calendarPage") {
-    return (
-      <div>
+        <LoginWithDiscord />
+      </>}
+      {context.page === "calendarPage" && <>
         <button onClick={() => setPage("landingPage")}>
           Navigate to LandingPage
         </button>
-        <LoginWithDiscord />
         <Calendar />
         <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
           <UserForm />
         </div>
-      </div>
-    )
-  }
+      </>}
+    </Context.Provider>
+  )
 }
 
 export default App;
