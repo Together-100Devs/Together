8b309dfa78c7e2978846af817aaf7929fd04d1c9
Matthew Bozin <mbozin94@gmail.com>
2022-12-05 13:26:57 -0800

    Added conditional page rendering based on Context in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,34 +6,22 @@ import DataService from "services/dataService";
 import { Context } from "./contexts/Context"
 
 function App() {
-  const [context, setContext] = useState({user: null, page: "landingPage"})
-
-  const setPage = (page) => {
-    context.page = page
-    setContext({...context})
-  }
+  const [context, setContext] = useState({user: null})
 
   useEffect(() => {
     DataService.getCurrentUser().then(response => {
       context.user = response.data;
       setContext({...context})
-      console.log(context.user)
     });
   }, []);
 
   return (
     <Context.Provider value={[context, setContext]}>
-      {context.page === "landingPage" && <>
+      <LoginWithDiscord />
+      {!context.user && <>
         <h1>Hello, landingPage goes here</h1>
-        <button onClick={() => setPage("calendarPage")}>
-          Navigate to Calendar
-        </button>
-        {!context.user && <LoginWithDiscord />}
       </>}
-      {context.page === "calendarPage" && <>
-        <button onClick={() => setPage("landingPage")}>
-          Navigate to LandingPage
-        </button>
+      {context.user && <>
         <Calendar />
         <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
           <UserForm />
