1412949110277916595eb4c5d2d9a3bd4f08d69a
Ricardo Hernandez <rmhm.photography@gmail.com>
2022-12-05 10:06:53 -0400

    feat: created SPA routing for rendering landing page or calendar page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Calendar from "features/calendar/Calendar";
 import LoginWithDiscord from "features/auth/LoginWithDiscord";
 import UserForm from "features/form/UserForm";
 
 function App() {
-  return (
-    <div>
-      <LoginWithDiscord />
-      <Calendar />
-      <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
-        <UserForm />
+  const [page, setPage] = useState("landingPage");  
+
+  if (page === "landingPage") {
+    return (
+      <>
+        <h1>Hello, landingPage goes here</h1>
+        <button onClick={() => setPage("calendarPage")}>
+          Navigate to Calendar
+        </button>
+      </>
+    ) 
+  } else if (page === "calendarPage") {
+    return (
+      <div>
+        <button onClick={() => setPage("landingPage")}>
+          Navigate to LandingPage
+        </button>
+        <LoginWithDiscord />
+        <Calendar />
+        <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+          <UserForm />
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
