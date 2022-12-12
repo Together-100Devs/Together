b45fb8c35bb3c65fb05a660a1849d6e182b2534d
Joe Dravarol <joedravarol@gmail.com>
2022-12-12 05:22:48 +0000

    feat: Create utilities function to generate all recurring dates from HTML input

--- client/src/utilities/calendar.js
+++ client/src/utilities/calendar.js
@@ -1,5 +1,33 @@
 import parseISO from 'date-fns/parseISO'
 import format from 'date-fns/format';
+import eachDayOfInterval from 'date-fns/eachDayOfInterval'
+
+// const useDate = () => {
+//   const [date, setDate] = useState(new Date());
+//   const year = getYear(date);
+//   // Month in string format; e.g. 'November'
+//   const month = format(date, "LLLL");
+//   const daysInMonth = getDaysInMonth(date);
+//   // First day of the month; e.g. 'Tue'
+//   const firstDay = format(startOfMonth(date), "E");
+
+//   const getNextMonth = () => {
+//     setDate(prevDate => new Date(getYear(prevDate), getMonth(prevDate) + 1));
+//   };
+
+//   const getPreviousMonth = () => {
+//     setDate(prevDate => new Date(getYear(prevDate), getMonth(prevDate) - 1));
+//   };
+
+//   return {
+//     year,
+//     month,
+//     daysInMonth,
+//     firstDay,
+//     getNextMonth,
+//     getPreviousMonth,
+//   };
+// };
 
 export const getMatchMonth = (monthToMatch, events) => {
   if (!events.length) return [];
@@ -28,4 +56,43 @@ export const getEventsByDayNumber = (currentDay, allEvents) => {
 export const formatToLocalTime = date => {
   const isoDate = parseISO(date);
   return format(isoDate, 'p')
+}
+
+export const createDateInUTC = (htmlDate = '', htmlTime = '') => {
+  const [year, month, day] = htmlDate.split('-');
+  const [hour, minute] = htmlTime.split(':');
+
+  // Month is 0 based so we need to subtract 1
+  return new Date(Date.UTC(year, month - 1, day, hour, minute))
+}
+
+export const generateRecurringDatesArray = ({ initialDate, startTime, endDate, endTime}, event) => {
+  // Generate UTC time from HTML input (date and time).
+  const startUTC = createDateInUTC(initialDate, startTime);
+  const endUTC = createDateInUTC(endDate, endTime);
+  // Generate a range of dates in between initialDate & endDate (date-fns does not generate the time sadly)
+  const result = eachDayOfInterval({
+    start: startUTC,
+    end: endUTC,
+  })
+
+  // Create recurring dates array with events information
+  const dates = result.map(date => {
+    const [month, day, year] = format(date, 'P').split('/')
+    const htmlDateFormat = `${year}-${month}-${day}`;
+    
+    // Recreate date with time added
+    const newStartDate = createDateInUTC(htmlDateFormat, startTime);
+    const newEndDate = createDateInUTC(htmlDateFormat, endTime);
+
+    return {
+      title: event.title,
+      description: event.description,
+      startAt: newStartDate,
+      endAt: newEndDate,
+      location:event.location,
+    }
+  })
+
+  return dates
 }
