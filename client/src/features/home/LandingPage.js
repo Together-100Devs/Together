25c091a65c988e49fab2feab314ca3ab349cd055
Nim-droid-droid <levkoz2222@outlook.com>
2022-12-13 03:21:04 +0000

    landing page

--- client/src/features/home/LandingPage.js
+++ client/src/features/home/LandingPage.js
@@ -1,5 +1,23 @@
 import "../../index.css";
+import LongCard from "./LongCard";
+import SmallCard from "./SmallCard";
 
-const LandingPage = () => <div></div>;
+const LandingPage = () => (
+  <div className="flex flex-wrap">
+    <div className="flex flex-row text-center space-x-6">
+      <SmallCard
+        title={"together"}
+        img1={"../../togetherFavicon.ico"}
+        img2={"../../threeHumans.png"}
+      />
+
+      <SmallCard img3={"../../hooman.png"} title={"Welcome!"} />
+    </div>
+
+    <div className=" w-70">
+      <LongCard />
+    </div>
+  </div>
+);
 
 export default LandingPage;
