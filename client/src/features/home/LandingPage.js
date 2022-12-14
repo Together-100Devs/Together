9dd735710c1ec9a34e82d5a6f29914a51f646c8b
Matthew Bozin <mbozin94@gmail.com>
2022-12-13 18:10:20 -0800

    Move page state variable in App.js into context. Add context to LandingPage.js. Add buttons to change context.page to App.js and LandingPage.js that will move between the two pages.

--- client/src/features/home/LandingPage.js
+++ client/src/features/home/LandingPage.js
@@ -1,23 +1,34 @@
+import React, { useContext } from "react";
 import "../../index.css";
 import LongCard from "./LongCard";
 import SmallCard from "./SmallCard";
+import { Context } from "contexts/Context";
 
-const LandingPage = () => (
-  <div className="flex flex-wrap ">
-    <div className="flex flex-col content-center md:flex-row text-center gap-8 ">
-      <SmallCard
-        title={"together"}
-        img1={"../../togetherFavicon.ico"}
-        img2={"../../threeHumans.png"}
-      />
-      <SmallCard img3={"../../hooman.png"} title={"Welcome!"} />
-    </div>
+const LandingPage = () => {
+  const [context, setContext] = useContext(Context)
+  return (
+    <div className="flex flex-wrap ">
+      <button onClick={() => {
+        context.page = "calendarPage"
+        setContext({...context})
+      }}>
+        Navigate to Calendar Page
+      </button>
+      <div className="flex flex-col content-center md:flex-row text-center gap-8 ">
+        <SmallCard
+          title={"together"}
+          img1={"../../togetherFavicon.ico"}
+          img2={"../../threeHumans.png"}
+        />
+        <SmallCard img3={"../../hooman.png"} title={"Welcome!"} />
+      </div>
 
-    <div className=" min-w-[15.25rem] ">
-      <LongCard />
+      <div className=" min-w-[15.25rem] ">
+        <LongCard />
+      </div>
     </div>
-  </div>
-);
+  )
+}
 
 
 export default LandingPage;
