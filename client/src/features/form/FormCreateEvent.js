9c36640c3b55db83ceb990c30f13318caeaaf21a
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2022-12-21 22:03:23 -0500

    Fixed some logic with the end date maximum

--- client/src/features/form/FormCreateEvent.js
+++ client/src/features/form/FormCreateEvent.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { FormMoverContext } from "./contexts/FormMoverContext";
 import { Context } from "contexts/Context";
-import { format, add, parseISO } from 'date-fns';
+import { format, add, sub, parseISO } from 'date-fns';
 import { useEffect } from "react";
 
 // This Component gets the Title, Description, and the start/end Dates for the event
@@ -16,21 +16,47 @@ export default function FormCreateEvent() {
   // When something on the form changes, modify userData to have the new stuff from the form
   const handleChange = e => {
     const { name, value } = e.target;
-    
+
     // TODO: move this note to documentation
     // Debug testing what context even is, turns out it's the Discord User data.
     // console.log(context);
-    
+
     // Set the userData
     setUserData({ ...userData, [name]: value, discordName: context.user?.displayName });
   };
 
-  useEffect(()=>{ // Debug code
-    userData["initialDate"] ? console.log(format(add(parseISO(userData["initialDate"]), {days: 90}), 'yyyy-MM-dd')) : console.log("initialDate DNE");
-
-    // Idea: fail-safe where if start date goes back past the 90 day limit AFTER setting start and end date
-    // We auto-set the end date too to prevent an exploit
+  useEffect(() => { // Debug code
+    userData["initialDate"] ? console.log("End Date:", format(add(parseISO(userData["initialDate"]), { days: 90 }), 'yyyy-MM-dd')) : console.log("initialDate DNE");
   }, [userData])
+
+  const calculateStartDateMinimum = () => {
+    const today = new Date(); // establish "today" as a string value
+
+    if (!userData["finalDate"]) { // if endDate does not exist, startDate's minimum is today
+      return format(today, 'yyyy-MM-dd');
+
+    } else { // if endDate exists, startDate cannot be more than 90 days before endDate
+
+      // OR it defaults to newDate if the date 90 days before endDate is before or equal to newDate
+      const NinetyDaysBefore = sub(parseISO(userData["finalDate"]), {days : 90});
+
+      if (NinetyDaysBefore <= today) { // NOTE: can you compare dates this way? who knows.
+        return format(today, 'yyyy-MM-dd');
+      } else {
+        return format(NinetyDaysBefore, 'yyyy-MM-dd');
+      }
+
+    }
+  }
+
+  const calculateEndDateMinimum = () => {
+    if (userData["startDate"] !== undefined) { // start date exists, end date minimum = start date
+      return format(userData["startDate"], 'yyyy-MM-dd')
+
+    } else { // start date does not exist in any case, end date minimum = today
+      return format(new Date(), 'yyyy-MM-dd');
+    }
+  }
 
   return (
     // TITLE OF EVENT FIELD
@@ -81,8 +107,8 @@ export default function FormCreateEvent() {
             name="initialDate"
             placeholder="Start Date"
             className="p-1 px-2 appearance-none outline-non w-full text-gray-800"
-            required 
-            min={format(new Date(), 'yyyy-MM-dd')}
+            required
+            min={calculateStartDateMinimum()}
             max='2023-12-31'
           />
         </div>
@@ -101,10 +127,11 @@ export default function FormCreateEvent() {
             name="finalDate"
             placeholder="endDate"
             className="p-1 px-2 appearance-none outline-non w-full text-gray-800"
-            required 
-            min={format(new Date(), 'yyyy-MM-dd')}
+            required
+            // If start date exists, then the minimum for the end date is start date, otherwise the minimum end date is today
+            min={userData["initialDate"] ? userData["initialDate"] : format(new Date(), 'yyyy-MM-dd')}
             // If start date exists, then the end date max = start date + 90 days, otherwise end date max = last day of 2023
-            max = {userData["initialDate"] ? format(add(parseISO(userData["initialDate"]), {days: 90}), 'yyyy-MM-dd') : '2023-12-31'}
+            max={userData["initialDate"] ? format(add(parseISO(userData["initialDate"]), { days: 90 }), 'yyyy-MM-dd') : '2023-12-31'}
           />
         </div>
       </div>
--- client/src/features/form/UserForm.js
+++ client/src/features/form/UserForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 // Form components
 import { FormMoverContext } from "./contexts/FormMoverContext";
