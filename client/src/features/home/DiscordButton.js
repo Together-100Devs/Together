83b95c3478d4c3253178b9a838213184f50d4db3
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-12-23 03:33:18 +0000

    Change button element to div, remove console warning, fix #197

--- client/src/features/home/DiscordButton.js
+++ client/src/features/home/DiscordButton.js
@@ -4,12 +4,11 @@ import LoginWithDiscord from "features/auth/LoginWithDiscord";
 const DiscordButton = () => (
   <>
     <div className=" flex -mx-10 border-2 border-slate-400 bg-[#b5c8d3] py-[0.1px] rounded">
-      <button className="md:px-10 px-2 py-1 my-3 mx-auto bg-discordBtn rounded text-base font-bold">
+      <div className="md:px-10 px-2 py-1 my-3 mx-auto bg-discordBtn rounded text-base font-bold">
         <LoginWithDiscord />
-      </button>
+      </div>
     </div>
   </>
 );
 
 export default DiscordButton;
-
