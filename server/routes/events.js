f2c9e5102bc7b6b89cb3d424b724c5256571730b
Caleb Cohen <CalebCohen@hotmail.com>
2022-12-31 22:31:55 -0600

    added ensure auth to the creation and deletion of events

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const eventsController = require("../controllers/events");
+const auth = require("../middleware/auth");
+
 
 //test function
 router.get("/ping", eventsController.ping);
 
-router.post("/create", eventsController.create);
+router.post("/create", auth.ensureAuth, eventsController.create);
 
 router.get("/", eventsController.getAll);
 
 router.get("/:id", eventsController.getOne);
 
-router.delete("/:id", eventsController.deleteEvent)
+router.delete("/:id", auth.ensureAuth, eventsController.deleteEvent)
 
-router.delete("/deleteAllEvents/:groupId", eventsController.deleteAllEvents)
+router.delete("/deleteAllEvents/:groupId", auth.ensureAuth, eventsController.deleteAllEvents)
 
 module.exports = router;
