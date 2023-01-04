613234077e6a5c5a1886f67234fd0dbe883f9524
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-01-04 02:36:34 +0000

    add a utility method httpError for creating errors

---
+++ server/utilities/httpError.js
@@ -0,0 +1,15 @@
+const errorMessages = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not found",
+  409: "Conflict",
+};
+
+const httpError = (status, message = errorMessages[status]) => {
+  const error = new Error(message);
+  error.status = status;
+  return error;
+};
+
+module.exports = httpError;
