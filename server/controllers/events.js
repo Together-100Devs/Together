d42f18f412144576da575ac17df519195bfd7336
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-01-04 02:37:25 +0000

    update getOne event controller

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,4 +1,6 @@
+const mongoose = require("mongoose");
 const Event = require("../models/Event");
+const httpError = require("../utilities/httpError");
 
 module.exports = {
   //test function
@@ -8,7 +10,7 @@ module.exports = {
   create: async (req, res) => {
     try {
       let data = JSON.parse(req.body.data);
-      data.forEach(obj => obj.user = req.user._id);
+      data.forEach(obj => (obj.user = req.user._id));
       await Event.insertMany(data);
       res.json({ message: "Event created!" });
     } catch (err) {
@@ -17,19 +19,32 @@ module.exports = {
   },
   getAll: async (req, res) => {
     try {
-      const events = await Event.find().populate('user').exec();
+      const events = await Event.find().populate("user").exec();
       // return all events
       res.json(events);
     } catch (err) {
       console.log(err);
     }
   },
-  getOne: async (req, res) => {
+  getOne: async (req, res, next) => {
     try {
-      const event = await Event.findById(req.params.id);
+      const { id } = req.params;
+
+      // Check if the ID is valid
+      if (!mongoose.isValidObjectId(id)) {
+        throw httpError(404);
+      }
+
+      const event = await Event.findById(id);
+
+      // Check if event exists
+      if (!event) {
+        throw httpError(404);
+      }
+      
       res.json(event);
     } catch (err) {
-      console.log(err);
+      next(err);
     }
   },
   deleteEvent: async (req, res) => {
@@ -37,9 +52,13 @@ module.exports = {
       const eventId = req.params.id;
       //checks if an event exists that _id, user, and req.user._id match. This is to prevent users that are authenticated from deleting events they do not author.
       const event = await Event.findOne({ _id: eventId, user: req.user._id });
-      if (!event) { return res.status(401).send({ message: 'You are not the author of this event' }); }
+      if (!event) {
+        return res
+          .status(401)
+          .send({ message: "You are not the author of this event" });
+      }
       await Event.deleteOne({ _id: eventId });
-      res.json({ message: 'Event deleted' });
+      res.json({ message: "Event deleted" });
     } catch (error) {
       console.error(error);
       res.send(500);
@@ -49,10 +68,17 @@ module.exports = {
     try {
       const groupId = req.params.groupId;
       //checks if an event exists that _id, user, and req.user._id match. This is to prevent users that are authenticated from deleting events they do not author.
-      const event = await Event.findOne({ groupId: groupId, user: req.user._id })
-      if (!event) { return res.status(401).send({ message: 'You are not the author of this event' }); }
+      const event = await Event.findOne({
+        groupId: groupId,
+        user: req.user._id,
+      });
+      if (!event) {
+        return res
+          .status(401)
+          .send({ message: "You are not the author of this event" });
+      }
       await Event.deleteMany({ groupId: groupId });
-      res.json({ message: 'Events deleted' });
+      res.json({ message: "Events deleted" });
     } catch (error) {
       console.error(error);
       res.send(500);
