b97105b61c8902321d83d8c9e9a0886bf2c6488f
Marlon Clay <luciousmc@icloud.com>
2023-01-07 14:17:59 -0800

    feat: add click handlers for header buttons
    + add Logout/Login functionalityt to calendar
    + Logout/Login Icon will change based on login status

--- client/src/features/calendarHeader/CalendarHeader.js
+++ client/src/features/calendarHeader/CalendarHeader.js
@@ -1,23 +1,74 @@
 import HeaderButton from "./components/HeaderButton";
 import { MdGroupAdd } from "react-icons/md";
 import { IoChatbubblesOutline } from "react-icons/io5";
-import { GrTableAdd } from "react-icons/gr";
 import { BsCalendarPlusFill } from "react-icons/bs";
 import { FaQuestion } from "react-icons/fa";
-import { RiArrowLeftCircleFill } from "react-icons/ri";
+import { RiArrowLeftCircleFill, RiArrowRightCircleFill } from "react-icons/ri";
+import MonthAndYear from "features/calendar/MonthAndYear";
+import useDate from "hooks/useDate";
+import Logo from "../../assets/images/togetherLogo.svg";
+import DataService from "services/dataService";
+import { useContext } from "react";
+import { Context } from "contexts/Context";
 
 function CalendarHeader() {
+  const date = useDate();
+  const [context, setContext] = useContext(Context);
+
+  const handleJoinClick = () => {
+    window.open(
+      "discord://discord.com/channels/735923219315425401/1038482732633825442",
+      "_blank"
+    );
+  };
+
+  const handleLogin = () => {
+    window.location = "/auth/discord";
+  };
+
+  const handleLogout = () => {
+    DataService.logout();
+    setContext(context => ({
+      ...context,
+      user: null,
+    }));
+  };
+
   return (
     <header className="flex items-center px-5 py-2 bg-white justify-between">
       <section className="flex space-x-2">
         <HeaderButton Icon={BsCalendarPlusFill} toolTipText="Add Event" />
-        <HeaderButton Icon={MdGroupAdd} toolTipText="Join Team" />
+        <HeaderButton
+          Icon={MdGroupAdd}
+          toolTipText="Join Team"
+          onClickFn={handleJoinClick}
+        />
       </section>
-      <section>Month Component</section>
+      <section className="flex items-center">
+        <img src={Logo} className="w-14" alt="Logo" />
+        <MonthAndYear
+          month={date.month}
+          year={date.year}
+          handleNextMonth={date.getNextMonth}
+          handlePreviousMonth={date.getPreviousMonth}
+        />
+      </section>
       <section className="flex space-x-2">
         <HeaderButton Icon={IoChatbubblesOutline} toolTipText="Feedback" />
         <HeaderButton Icon={FaQuestion} toolTipText="Help" />
-        <HeaderButton Icon={RiArrowLeftCircleFill} toolTipText="Logout" />
+        {context.user ? (
+          <HeaderButton
+            onClickFn={handleLogout}
+            Icon={RiArrowLeftCircleFill}
+            toolTipText="Logout"
+          />
+        ) : (
+          <HeaderButton
+            onClickFn={handleLogin}
+            Icon={RiArrowRightCircleFill}
+            toolTipText="Login"
+          />
+        )}
       </section>
     </header>
   );
--- client/src/features/calendarHeader/components/HeaderButton.js
+++ client/src/features/calendarHeader/components/HeaderButton.js
@@ -1,8 +1,11 @@
-function HeaderButton({ Icon, toolTipText }) {
+function HeaderButton({ Icon, toolTipText, onClickFn }) {
   return (
-    <button className="relative p-3 bg-[#F5E7DE] border-2 border-black rounded-lg group font-inconsolata font-bold">
+    <button
+      onClick={onClickFn}
+      className="relative p-3 bg-[#F5E7DE] border-2 border-black rounded-lg group font-inconsolata font-bold"
+    >
       <Icon className="w-8 h-8 text-[#C57756]" />
-      <span className="absolute -bottom-full left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-[200ms] ease-in bg-[#F5CEB5D9] p-2 border border-black whitespace-nowrap rounded-md pointer-events-none">
+      <span className="absolute -bottom-full left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-[200ms] ease-in bg-[#F5CEB5BF] p-2 border border-black whitespace-nowrap rounded-md pointer-events-none">
         {toolTipText}
       </span>
     </button>
