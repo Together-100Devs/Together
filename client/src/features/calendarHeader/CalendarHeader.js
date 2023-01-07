62d417c52b3b13b82c412092fb86ce4227d8c1fd
Marlon Clay <luciousmc@icloud.com>
2023-01-07 11:21:59 -0800

    feat: add header button component

--- client/src/features/calendarHeader/CalendarHeader.js
+++ client/src/features/calendarHeader/CalendarHeader.js
@@ -1,15 +1,23 @@
+import HeaderButton from "./components/HeaderButton";
+import { MdGroupAdd } from "react-icons/md";
+import { IoChatbubblesOutline } from "react-icons/io5";
+import { GrTableAdd } from "react-icons/gr";
+import { BsCalendarPlusFill } from "react-icons/bs";
+import { FaQuestion } from "react-icons/fa";
+import { RiArrowLeftCircleFill } from "react-icons/ri";
+
 function CalendarHeader() {
   return (
-    <header className="flex items-center p-5 bg-white justify-between">
-      <section>
-        <button>Add Event</button>
-        <button>Join TEam</button>
+    <header className="flex items-center px-5 py-2 bg-white justify-between">
+      <section className="flex space-x-2">
+        <HeaderButton Icon={BsCalendarPlusFill} toolTipText="Add Event" />
+        <HeaderButton Icon={MdGroupAdd} toolTipText="Join Team" />
       </section>
       <section>Month Component</section>
-      <section>
-        <button>Feedback</button>
-        <button>Help</button>
-        <button>Logout</button>
+      <section className="flex space-x-2">
+        <HeaderButton Icon={IoChatbubblesOutline} toolTipText="Feedback" />
+        <HeaderButton Icon={FaQuestion} toolTipText="Help" />
+        <HeaderButton Icon={RiArrowLeftCircleFill} toolTipText="Logout" />
       </section>
     </header>
   );
