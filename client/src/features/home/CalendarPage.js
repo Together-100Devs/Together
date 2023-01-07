50de150530fa386d1fdf0145fa3609949ad65e00
Marlon Clay <luciousmc@icloud.com>
2023-01-07 15:05:01 -0800

    remove lady svg

--- client/src/features/home/CalendarPage.js
+++ client/src/features/home/CalendarPage.js
@@ -1,12 +1,10 @@
-// import readingLadySVG from "../../assets/images/readingLady.svg";
 import Calendar from "../calendar/Calendar";
 import CalendarHeader from "../calendarHeader";
 
 function CalendarPage() {
   return (
-    <main className="flex flex-col gap-3 p-5 shadow-sm">
+    <main className="flex flex-col gap-3 p-3 shadow-sm">
       <CalendarHeader />
-
       <Calendar />
     </main>
   );
