89a688ccc47fc540d90ff7fdf5d7cdff9a4664f7
Marlon Clay <luciousmc@icloud.com>
2023-01-07 11:23:11 -0800

    refactor: move icons to header component

--- client/src/features/home/CalendarPage.js
+++ client/src/features/home/CalendarPage.js
@@ -1,16 +1,10 @@
 // import readingLadySVG from "../../assets/images/readingLady.svg";
-import { MdGroupAdd } from "react-icons/md";
-import { IoChatbubblesOutline } from "react-icons/io5";
-import { GrTableAdd } from "react-icons/gr";
-import { BsCalendarPlusFill } from "react-icons/bs";
-import { FaQuestion } from "react-icons/fa";
-import { RiArrowLeftCircleFill } from "react-icons/ri";
 import Calendar from "../calendar/Calendar";
 import CalendarHeader from "../calendarHeader";
 
 function CalendarPage() {
   return (
-    <main className="flex flex-col gap-3 p-3 shadow-sm">
+    <main className="flex flex-col gap-3 p-5 shadow-sm">
       <CalendarHeader />
 
       <Calendar />
