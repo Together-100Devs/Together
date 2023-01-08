463d128bc28e9d38bda695534676c7b5d00a8495
Marlon Clay <luciousmc@icloud.com>
2023-01-08 14:10:08 -0800

    style: change duration of tooltip appearance

--- client/src/features/calendarHeader/components/HeaderButton.js
+++ client/src/features/calendarHeader/components/HeaderButton.js
@@ -1,12 +1,12 @@
-function HeaderButton({ Icon, toolTipText, onClickFn }) {
+function HeaderButton({ Icon, tooltipText, onClickFn }) {
   return (
     <button
       onClick={onClickFn}
       className="relative p-3 bg-[#F5E7DE] border-2 border-black rounded-lg group font-inconsolata font-bold"
     >
       <Icon className="w-8 h-8 text-[#C57756]" />
-      <span className="absolute -bottom-full left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-[200ms] ease-in bg-[#F5CEB5BF] p-2 border border-black whitespace-nowrap rounded-md pointer-events-none">
-        {toolTipText}
+      <span className="absolute -bottom-full left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-in bg-[#F5CEB5BF] p-2 border border-black whitespace-nowrap rounded-md pointer-events-none">
+        {tooltipText}
       </span>
     </button>
   );
