154734490e507ef2f123d1f57e522abd0f76f449
Marlon Clay <luciousmc@icloud.com>
2023-01-08 14:12:40 -0800

    fix: update login/logout to updated context from merge

--- client/src/features/calendarHeader/CalendarHeader.js
+++ client/src/features/calendarHeader/CalendarHeader.js
@@ -6,65 +6,50 @@ import { FaQuestion } from "react-icons/fa";
 import { RiArrowLeftCircleFill, RiArrowRightCircleFill } from "react-icons/ri";
 import MonthAndYear from "features/calendar/MonthAndYear";
 import Logo from "../../assets/images/togetherLogo.svg";
-import DataService from "services/dataService";
-import { useContext } from "react";
-import { Context } from "contexts/Context";
+import { useAuthContext } from "contexts/AuthContext";
 
 function CalendarHeader({ date }) {
-  const [context, setContext] = useContext(Context);
-
-  const handleJoinClick = () => {
-    window.open(
-      "discord://discord.com/channels/735923219315425401/1038482732633825442",
-      "_blank"
-    );
-  };
+  const { isAuthenticated, logout } = useAuthContext();
+  const TOGETHER_THREAD_URL =
+    "discord://discord.com/channels/735923219315425401/1038482732633825442";
 
   const handleLogin = () => {
     window.location = "/auth/discord";
   };
 
-  const handleLogout = () => {
-    DataService.logout();
-    setContext(context => ({
-      ...context,
-      user: null,
-    }));
-  };
-
   return (
     <header className="flex items-center px-5 py-2 bg-white justify-between">
       <section className="flex space-x-3">
-        <HeaderButton Icon={BsCalendarPlusFill} toolTipText="Add Event" />
+        <HeaderButton Icon={BsCalendarPlusFill} tooltipText="Add Event" />
         <HeaderButton
           Icon={MdGroupAdd}
-          toolTipText="Join Team"
-          onClickFn={handleJoinClick}
+          tooltipText="Join Team"
+          onClickFn={() => window.open(TOGETHER_THREAD_URL, "_blank")}
         />
       </section>
       <section className="flex items-center">
         <img src={Logo} className="w-14" alt="Logo" />
         <MonthAndYear
-          month={date.month}
-          year={date.year}
-          handleNextMonth={date.getNextMonth}
-          handlePreviousMonth={date.getPreviousMonth}
+          month={date?.month}
+          year={date?.year}
+          handleNextMonth={date?.getNextMonth}
+          handlePreviousMonth={date?.getPreviousMonth}
         />
       </section>
       <section className="flex space-x-3">
-        <HeaderButton Icon={IoChatbubblesOutline} toolTipText="Feedback" />
-        <HeaderButton Icon={FaQuestion} toolTipText="Help" />
-        {context.user ? (
+        <HeaderButton Icon={IoChatbubblesOutline} tooltipText="Feedback" />
+        <HeaderButton Icon={FaQuestion} tooltipText="Help" />
+        {isAuthenticated() ? (
           <HeaderButton
-            onClickFn={handleLogout}
             Icon={RiArrowLeftCircleFill}
-            toolTipText="Logout"
+            tooltipText="Logout"
+            onClickFn={logout}
           />
         ) : (
           <HeaderButton
-            onClickFn={handleLogin}
             Icon={RiArrowRightCircleFill}
-            toolTipText="Login"
+            tooltipText="Login"
+            onClickFn={handleLogin}
           />
         )}
       </section>
