58bae868c6354eaa4a016848abc6d4240b1ffef9
Marlon Clay <luciousmc@icloud.com>
2023-01-08 14:13:51 -0800

    chore: update code to updated contexts

--- client/src/features/home/CalendarPage.js
+++ client/src/features/home/CalendarPage.js
@@ -1,15 +1,33 @@
 import useDate from "hooks/useDate";
 import Calendar from "../calendar/Calendar";
 import CalendarHeader from "../calendarHeader";
+import FormProvider from "contexts/FormContext";
+import { useRoutingContext } from "contexts/RoutingContext";
+import Modal from "features/modal/Modal";
+import UserForm from "features/form/UserForm";
+import { useAuthContext } from "contexts/AuthContext";
 
 function CalendarPage() {
+  const routing = useRoutingContext();
+  const auth = useAuthContext();
   const date = useDate();
 
   return (
-    <main className="flex flex-col gap-3 p-3 shadow-sm min-h-screen max-w-[1920px] mx-auto">
-      <CalendarHeader date={date} />
-      <Calendar date={date} />
-    </main>
+    <>
+      <button onClick={() => routing.setCurrentPage("landingPage")}>
+        Navigate to LandingPage
+      </button>
+      <main className="flex flex-col gap-3 p-3 shadow-sm min-h-screen max-w-[1920px] mx-auto">
+        <CalendarHeader date={date} />
+        <Calendar date={date} />
+      </main>
+      <FormProvider>
+        <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+          <Modal type={"event"} open={"eventModal"} />
+          {auth?.user && <UserForm />}
+        </div>
+      </FormProvider>
+    </>
   );
 }
 export default CalendarPage;
