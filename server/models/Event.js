04956ef7d6bc0de702e05529a943a88969ba1118
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-01-10 01:40:35 +0000

    validate: endAt - startAt must be 3 month max

--- server/models/Event.js
+++ server/models/Event.js
@@ -44,8 +44,20 @@ const createEventSchema = Joi.array().items(
     description: Joi.string().required(),
     location: Joi.string().required(),
     groupId: Joi.string().allow(null),
-    startAt: Joi.date().timestamp().greater(Date.now()).required(),
-    endAt: Joi.date().timestamp().greater(Joi.ref("startAt")).required(),
+    startAt: Joi.date().timestamp().greater("now").required(),
+    endAt: Joi.date()
+      .timestamp()
+      .greater(Joi.ref("startAt"))
+      .less(
+        Joi.ref("startAt", {
+          adjust: timestamp => {
+            let date = new Date(timestamp);
+            date.setMonth(date.getMonth() + 3);
+            return date;
+          },
+        })
+      )
+      .required(),
   })
 );
 
