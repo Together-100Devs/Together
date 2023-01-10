fec39b204f932f0c1228963915d7def2d860766a
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-01-10 15:59:59 +0000

    Revert "make deserializeUser async"
    This reverts commit 334b83dd6ed35b4a95be86e6981ba31721f7ee05.

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,4 +1,4 @@
-const DiscordStrategy = require("passport-discord");
+const DiscordStrategy = require("passport-discord").Strategy;
 const User = require("../models/User");
 
 module.exports = function (passport) {
@@ -6,14 +6,8 @@ module.exports = function (passport) {
     done(null, user.id);
   });
 
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const user = await User.findById(id);
-      if (!user) throw new Error("User not found");
-      done(null, user);
-    } catch (err) {
-      done(err);
-    }
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
   });
 
   //Discord authentication
