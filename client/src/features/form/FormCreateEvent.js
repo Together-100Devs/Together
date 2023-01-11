23edce208d55c93ea823a9fe940c70f11984b08a
isaaclee12 <59675296+isaaclee12@users.noreply.github.com>
2023-01-10 20:08:16 -0500

    One last item linted out

--- client/src/features/form/FormCreateEvent.js
+++ client/src/features/form/FormCreateEvent.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useFormContext } from "contexts/FormContext";
 import { useAuthContext } from "contexts/AuthContext";
 
