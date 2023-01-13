e2cf373780997010f33df4398109262edc67326e
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-01-13 18:53:44 +0000

    remove unused utility methods

--- server/utilities/dateToTimestamp.js
+++
@@ -1,9 +0,0 @@
-/**
- * Convert date and time from a form to Javascript timestamp
- * @param {string} date 'yyyy-mm-dd'
- * @param {string} time 'hh:mm'
- * @returns {Date}
- */
-const dateToTimestamp = (date, time) => new Date(`${date}T${time}`).getTime();
-
-module.exports = { dateToTimestamp };
