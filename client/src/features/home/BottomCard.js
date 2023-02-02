5b479d1411628a1cc17b33c722adb7b6d0f4059f
Caleb Cohen <75268174+Caleb-Cohen@users.noreply.github.com>
2023-02-02 09:48:42 -0600

    nestedParagraphs without package-lock Co-authored By: David Rodriguez <David.Rod1865@gmail.com> (#290)

--- client/src/features/home/BottomCard.js
+++ client/src/features/home/BottomCard.js
@@ -5,10 +5,10 @@ const BottomCard = ({ heading, title, description, img }) => (
     <section className="tablet:flex tablet:justify-between h-fit border-solid border-2 border-black rounded-lg shadow-lg bg-secondary py-16 tablet:pt-10 px-6 tab">
       <div className=" tablet:w-2/3 tablet:px-20">
         <h3 className=" text-center text-2xl font-bold pb-10">{heading}</h3>
-        <p className="font-semibold text-sm tablet:text-base laptop:text-lg">
+        <div className="font-semibold text-sm tablet:text-base laptop:text-lg">
           <span className="font-bold">{title}</span>
           {description}
-        </p>
+        </div>
       </div>
       <div className=" tablet:w-1/3 self-center tablet:pr-10 "> 
         <img src={img} alt="two people sitting" />
