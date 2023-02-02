195df46b839e05a8a6e613c2be14295c9afe8947
Roman Stetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-02-02 16:47:28 +0000

    Feature/issue 309 (#311)
    * add user's timezone to event modal
    
    * run prettier

--- client/src/features/form/FormScheduleEvent.js
+++ client/src/features/form/FormScheduleEvent.js
@@ -12,7 +12,11 @@ export default function FormScheduleEvent() {
 
     // Set the value of the end date equal to start date's value when the start date's value is change, but NOT vice versa.
     if (name === "initialDate" && formData.recurring.rate === "noRecurr") {
-      setFormData(prevFormData => ({ ...prevFormData, "initialDate": value, "finalDate": value }));
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        initialDate: value,
+        finalDate: value,
+      }));
       return;
     }
 
@@ -21,24 +25,45 @@ export default function FormScheduleEvent() {
 
   return (
     <div>
-
-
       {formScheduleEventErrors.map((error, index) => {
-        return <div class="alert alert-error shadow-lg text-red-700" key="index">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            <span>{error}</span>
+        return (
+          <div class="alert alert-error shadow-lg text-red-700" key="index">
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="stroke-current flex-shrink-0 h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>{error}</span>
+            </div>
           </div>
-        </div>
+        );
       })}
 
       <div>
         <FormRecurringDates />
       </div>
 
-      <p className="text-xs mt-10 ">Note: Start Date and End Date cannot be more than 90 days apart, and both must be in 2023.</p>
-      <div className="grid grid-cols-2">
+      <p className="text-xs mt-10 ">
+        Note: Start Date and End Date cannot be more than 90 days apart, and
+        both must be in 2023.
+      </p>
 
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-grey-500 text-xs leading-8 uppercase">
+          Timezone: {Intl.DateTimeFormat().resolvedOptions().timeZone}
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2">
         {/* START DATE OF EVENT */}
         <div className="w-full mx-2 flex-1">
           {/* Note for user to understand the date boundaries. Added margin to seperate the date section from the event info */}
@@ -54,8 +79,8 @@ export default function FormScheduleEvent() {
               placeholder="Start Date"
               className="p-1 px-2 appearance-none outline-non w-full text-gray-800"
               required
-              min={format(new Date(), 'yyyy-MM-dd')}
-              max='2023-12-31'
+              min={format(new Date(), "yyyy-MM-dd")}
+              max="2023-12-31"
             />
           </div>
         </div>
@@ -90,8 +115,8 @@ export default function FormScheduleEvent() {
               placeholder="endDate"
               className="p-1 px-2 appearance-none outline-non w-full text-gray-800"
               required
-              min={format(new Date(), 'yyyy-MM-dd')}
-              max='2023-12-31'
+              min={format(new Date(), "yyyy-MM-dd")}
+              max="2023-12-31"
             />
           </div>
         </div>
@@ -111,8 +136,6 @@ export default function FormScheduleEvent() {
           </div>
         </div>
       </div>
-
-
     </div>
   );
 }
