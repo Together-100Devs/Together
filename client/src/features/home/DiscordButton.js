8bd20c33c0461c92932a5bb8adbbd8fdc9e3008b
Caleb Cohen <75268174+Caleb-Cohen@users.noreply.github.com>
2023-02-02 09:47:32 -0600

    Removed button (#286)
    Co-authored-by: Rhyse <dev.rhyse@gmail.com>

--- client/src/features/home/DiscordButton.js
+++ client/src/features/home/DiscordButton.js
@@ -5,9 +5,9 @@ import { FaDiscord } from "react-icons/fa";
 const DiscordButton = () => (
   <div className="flex justify-center pt-10 tablet:pt-5">
     <div className=" flex w-64 tablet:w-auto border-2 border-slate-400 bg-white rounded-xl ">
-      <button className="px-2 tablet:px-10 py-2 m-3 bg-discordBtn rounded-lg tablet:text-xl font-bold">
-        <LoginWithDiscord DiscordIcon={FaDiscord} />
-      </button>
+      <div className="px-2 tablet:px-10 py-2 m-3 bg-discordBtn rounded-lg tablet:text-xl font-bold">
+          <LoginWithDiscord DiscordIcon={FaDiscord}/>
+      </div>
     </div>
   </div>
 );
