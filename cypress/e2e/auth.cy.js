c2e64c44cb4a153a3a7b0cde2c647d62fe8a575a
Rascal Two <therealrascaltwo@gmail.com>
2023-02-11 14:32:36 -0500

    Feature/issue 314 (#319)
    * Install Cypress & needed packages
    
    Update eslint config
    Update gitignore
    Add testing development mongo path
    
    * Add nock Passport.js Discord request interception
    
    * Add Cypress tests
    
    * Disable server logging when testing
    
    * Fix delay-toggling environment variable
    
    Cypress wasn't properly listening to the CYPRESS_NO_DELAYS environment variable to disable the animations waits
    
    * added modulus to getHoursAndMinutes
    
    ---------
    
    Co-authored-by: Caleb Cohen <CalebCohen@hotmail.com>

---
+++ cypress/e2e/auth.cy.js
@@ -0,0 +1,75 @@
+/// <reference types="cypress" />
+
+import { getMonthAndYear } from "../support/functions";
+import tgt from "../support/tgt";
+
+describe("Discord OAuth works", () => {
+  it("100Dever is allowed in & back", () => {
+    cy.login("100_DEVER", true);
+    tgt.auth.button.login().should("not.exist");
+    tgt.calendar.currentMonthAndYear().contains(getMonthAndYear());
+
+    tgt.auth.button.logout().click();
+    cy.login("100_DEVER");
+
+    tgt.auth.button.login().should("not.exist");
+    tgt.calendar.currentMonthAndYear().contains(getMonthAndYear());
+  });
+
+  describe("Welcome Model", () => {
+    it("Is shown until dismissed", () => {
+      cy.login("100_DEVER");
+      tgt.modal.get().contains("Hello, 100Dever#0001");
+
+      cy.reload();
+      tgt.modal.get().contains("Hello, 100Dever#0001");
+    });
+
+    for (const backdrop of [false, true]) {
+      it(`Dismissible via ${backdrop ? "backdrop" : "close button"}`, () => {
+        cy.login("100_DEVER");
+        tgt.modal.get().contains("Hello, 100Dever#0001");
+        tgt.modal.close(backdrop);
+
+        cy.reload();
+        tgt.modal.get().should("not.exist");
+      });
+    }
+  });
+
+  for (const button of ["Login with Discord", "Log In"]) {
+    it(`"${button}" button works`, () => cy.login("100_DEVER", false, button));
+  }
+
+  it(`Calendar "Login" button works`, () => {
+    tgt.landing.button.calendar().click();
+    cy.login("100_DEVER", false, "Login");
+  });
+
+  it("Rejection for outsiders", () => {
+    cy.login("JOHN_DOE");
+    tgt.modal.get().contains("only 100Devs users are allowed");
+    tgt.modal.close();
+    tgt.auth.button.login().should("exist");
+  });
+
+  it("Logout from Calendar", () => {
+    cy.login("100_DEVER", true);
+    tgt.auth.button.logout().click();
+    tgt.auth.button.login().should("exist");
+  });
+
+  it("Log Out from Landing", () => {
+    cy.login("100_DEVER", true);
+    tgt.nav.button.home().click();
+    tgt.auth.button.logout("Logout").click();
+    tgt.auth.button.login().should("exist");
+  });
+
+  it("Logout from Landing", () => {
+    cy.login("100_DEVER", true);
+    tgt.nav.button.home().click();
+    tgt.auth.button.logout("Log Out").click();
+    tgt.auth.button.login().should("exist");
+  });
+});
---
+++ cypress/e2e/landing.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+import tgt from "../support/tgt";
+
+describe("Landing", () => {
+  it("Home links to home", () => {
+    cy.visit("/");
+    tgt.nav.button.home().click();
+    cy.url().should("eq", Cypress.config().baseUrl + "/");
+  });
+
+  it("GitHub link exists", () => {
+    cy.contains('a[href^="https://github.com"]', "GitHub");
+  });
+
+  it("Hamburger Menu", () => {
+    cy.viewport("iphone-6");
+    tgt.nav.hamburger.toggle().click();
+    tgt.nav.hamburger.menu().contains("CALENDAR").should("be.visible");
+    tgt.nav.hamburger.toggle().click();
+    tgt.nav.hamburger.menu().contains("CALENDAR").should("not.be.visible");
+  });
+});
