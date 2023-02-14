3a116f7a04bdf804e9602b4b6c7279ff8f57ce8f
lalysi <mountainsims@gmail.com>
2023-02-14 10:33:24 -0700

    Removed delete all button from non-recurring events

--- client/src/features/modal/EventModal.jsx
+++ client/src/features/modal/EventModal.jsx
@@ -12,12 +12,12 @@ import { useAuthContext } from "contexts/AuthContext";
 
 const EventModal = () => {
   const modal = useModalContext();
-
   //grabs user compares user from context and event author
   //displays delete buttons if true
   const { user } = useAuthContext();
   const userId = user?._id;
   const authorCheck = userId === modal.activeEvent.user._id;
+  console.log(modal.activeEvent.groupId)
 
   return (
     <div className="flex flex-col items-center py-0 px-2rem rounded-xl bg-white pb-4">
@@ -39,7 +39,7 @@ const EventModal = () => {
           Delete Specific Event
         </button>
       )}
-      {authorCheck && (
+      {authorCheck && modal.activeEvent.groupId && (
         <button
           className="w-auto h-10 mt-5 px-2 border-solid border-2 border-gray outline-none rounded font-semibold text-xl hover:bg-teal-600 active:bg-teal-700 focus:outline-none focus:ring focus:ring-teal-300 inline-block"
           onClick={() =>
