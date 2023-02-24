a271e70177ff8069bdbab6b9ef6c46803d8c48cf
Jacqui Longhurst <63796776+jalonghurst@users.noreply.github.com>
2023-02-24 22:07:13 +0100

    Added comments to code changes

--- client/src/features/home/LongCard.js
+++ client/src/features/home/LongCard.js
@@ -3,6 +3,7 @@ import DiscordButton from "./DiscordButton";
 import { useAuthContext } from "contexts/AuthContext";
 
 const LongCard = ({ heading, title, description, img }) => {
+  // Access authentication context
  const auth = useAuthContext();
  return (
     <div className="w-full p-4 ">
@@ -11,8 +12,9 @@ const LongCard = ({ heading, title, description, img }) => {
           <img src={img} alt="two people sitting" />
         </div>
         <div className="tablet:w-full">
+         {/* Display "heading, username" if logged in, or just the heading if logged out */}
           <h1 className="py-8 text-2xl font-bold text-center h-28 tablet:h-32 tablet:pt-14 tablet:text-xl">
-          {auth.user ? `${heading}, ${auth.user?.displayName}!` : `${heading}!`}
+           {auth.user ? `${heading}, ${auth.user?.displayName}!` : `${heading}!`}
           </h1>
           <p className="pl-10 pr-10 font-medium text-md tablet:text-xl">
             <span className="font-bold">{title}</span>
