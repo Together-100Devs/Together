fc25d8025e06ffa0090592116c22e44337a7aed3
calebco <calebcohen@hotmail.com>
2023-02-25 10:24:15 -0600

    updated linting for PR 353

--- client/src/features/home/LongCard.js
+++ client/src/features/home/LongCard.js
@@ -4,17 +4,19 @@ import { useAuthContext } from "contexts/AuthContext";
 
 const LongCard = ({ heading, title, description, img }) => {
   // Access authentication context
- const auth = useAuthContext();
- return (
+  const auth = useAuthContext();
+  return (
     <div className="w-full p-4 ">
       <section className="flex py-6 laptop:px-20 tablet:h-[44rem] border-solid border-2 border-black rounded-3xl bg-secondary tablet:py-20 l-fit">
         <div className="hidden tablet:w-full place-self-center tablet:block">
           <img src={img} alt="two people sitting" />
         </div>
         <div className="tablet:w-full">
-         {/* Display "heading, username" if logged in, or just the heading if logged out */}
+          {/* Display "heading, username" if logged in, or just the heading if logged out */}
           <h1 className="py-8 text-2xl font-bold text-center h-28 tablet:h-32 tablet:pt-14 tablet:text-xl">
-           {auth.user ? `${heading}, ${auth.user?.displayName}!` : `${heading}!`}
+            {auth.user
+              ? `${heading}, ${auth.user?.displayName}!`
+              : `${heading}!`}
           </h1>
           <p className="pl-10 pr-10 font-medium text-md tablet:text-xl">
             <span className="font-bold">{title}</span>
@@ -24,6 +26,6 @@ const LongCard = ({ heading, title, description, img }) => {
         </div>
       </section>
     </div>
-  )
+  );
 };
 export default LongCard;
