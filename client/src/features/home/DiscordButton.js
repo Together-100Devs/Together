2a7289ebecc82daea60d772da3fca691f0b6f0a0
neerav <57148990+neeravbhaskarla@users.noreply.github.com>
2023-07-26 00:23:50 +0530

    Bug/Issue 434 fixed discord and github login link clickable (#438)
    * bug: fixed discord and github login link clickable
    
    * updated the code with exisiting commits
    
    * Delete package.json
    
    * updated with new changes
    
    * Update package.json

--- client/src/features/home/DiscordButton.js
+++ client/src/features/home/DiscordButton.js
@@ -3,11 +3,9 @@ import LoginWithDiscord from "features/auth/LoginWithDiscord";
 import { FaDiscord } from "react-icons/fa";
 
 const DiscordButton = () => (
-  <div className="flex border-2 border-slate-400 bg-white rounded-2xl">
-    <div className="py-3 px-8 m-3 bg-discordBtn rounded-lg font-bold">
-      <LoginWithDiscord DiscordIcon={FaDiscord} />
-    </div>
-  </div>
+  <>
+    <LoginWithDiscord DiscordIcon={FaDiscord} />
+  </>
 );
 
 export default DiscordButton;
