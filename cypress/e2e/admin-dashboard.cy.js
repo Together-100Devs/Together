f86c7d42b466d37c6eea9f71611cf3c3e90f5705
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-07-27 23:37:55 -0400

    Refactor admin dashboard tests to add multiple events
    - The "Add Events" tests now accepts a list of event obects
    - Setup was also refactored into a `before()` and `beforeEach()` for consistency

--- cypress/e2e/admin-dashboard.cy.js
+++ cypress/e2e/admin-dashboard.cy.js
@@ -1,97 +1,92 @@
 /// <reference types="cypress" />
 
 import tgt from "../support/tgt";
-import {
-  createOffsetDate,
-  ensureInMiddleOfMonthAndDay,
-} from "../support/functions";
+
+// Generate test event
+const TEST_EVENT_AUTHOR = "100_DEVER";
+const TEST_EVENTS = [
+  {
+    title: "1st EVENT",
+    description: "1st Description",
+    location: "1st Location",
+    groupId: "admin_text_event_id",
+    startAt: Date.now(),
+    endAt: Date.now() + 3600,
+  },
+  {
+    title: "2nd EVENT",
+    description: "2nd Description",
+    location: "2nd Location",
+    groupId: "admin_text_event_id",
+    startAt: Date.now(),
+    endAt: Date.now() + 3600,
+  },
+];
+
+const addTestEvents = (test_events = TEST_EVENTS) => {
+  cy.url().then(return_url => {
+    cy.visit("/");
+
+    // Generate test events
+    cy.createOwnEvents(TEST_EVENT_AUTHOR, ...test_events);
+
+    // Close event creation modal
+    tgt.modal.close();
+
+    // Return to original URL
+    cy.visit(return_url);
+  });
+};
 
 describe("Admin Dashboard", () => {
+  const aliasHeaderEventCount = alias_name => {
+    cy.get("h1 .event-count")
+      .invoke("text")
+      .should("not.contain", "LOADING")
+      .then(parseInt)
+      .as(alias_name);
+  };
+
   it("Admin Dashboard displays admin dashboard header", () => {
     cy.visit("/admindashboard");
     cy.get("h1").contains("Admin Dashboard");
   });
 
-  describe("Updates when new events added", () => {
-    it("Displays new event on page refresh", () => {
-      // Ensure event container exists
-      cy.visit("/admindashboard");
-      cy.get("#events").should("exist");
-
-      const now = ensureInMiddleOfMonthAndDay();
-      const nextMonth = createOffsetDate(now, "Month", 1);
-
-      const test_title = "Test Title";
-      const test_description = "Test Description";
-      const test_location = "Test Location";
-      const test_author = "100_DEVER";
-
-      // Generate test event
-      cy.visit("/");
-      cy.createOwnEvents(test_author, {
-        title: test_title,
-        description: test_description,
-        location: test_location,
-        startAt: nextMonth,
-        endAt: new Date(nextMonth.getTime() + 1),
-        groupId: null,
-      });
-
-      // Close event creation modal
-      tgt.modal.close();
-
-      cy.visit("/admindashboard");
-      // Ensure test event rendered with correct title
-      cy.get("#events")
-        .find(".event li:nth-of-type(1)")
-        .invoke("text")
-        .should("contain", test_title);
-
-      // Ensure test event rendered with correct description
-      cy.get("#events")
-        .find(".event li:nth-of-type(2)")
-        .invoke("text")
-        .should("contain", test_description);
-    });
-
-    const aliasHeaderEventCount = alias_name => {
-      cy.get("h1 .event-count")
-        .invoke("text")
-        .should("not.contain", "LOADING")
-        .then(parseInt)
-        .as(alias_name);
-    };
-
-    describe("Add event", () => {
-      it("Event Counts", () => {
-        // Ensure event container exists
+  describe("Page updates when events change", () => {
+    describe("Add Events", () => {
+      before(() => {
+        // Navigate to admin dashboard
         cy.visit("/admindashboard");
         cy.get("#events").should("exist");
 
-        cy.visit("/admindashboard");
-        cy.get("h1").should("be.visible");
-
         // Create alias for initial event count
         aliasHeaderEventCount("initialEventCount");
-
-        // Generate test event
-        const now = ensureInMiddleOfMonthAndDay();
-        const nextMonth = createOffsetDate(now, "Month", 1);
-
-        cy.visit("/");
-        cy.createOwnEvents("100_DEVER", {
-          title: "Test Title",
-          description: "Test Description",
-          location: "Test Location",
-          startAt: nextMonth,
-          endAt: new Date(nextMonth.getTime() + 1),
-          groupId: null,
-        });
-        tgt.modal.close();
-
-        // Return to admin dashboard
+      });
+      beforeEach(() => {
+        // Navigate to admin dashboard
         cy.visit("/admindashboard");
 
+        // Add test events
+        addTestEvents(TEST_EVENTS);
+      });
+
+      it("Event titles and descriptions", () => {
+        TEST_EVENTS.forEach((test_event, i) => {
+          // Ensure test events rendered with correct title
+          cy.get("#events")
+            .find(`.event:nth-of-type(${i + 1}) li:nth-of-type(1)`)
+            .invoke("text")
+            .should("contain", test_event.title);
+
+          // Ensure test event rendered with correct description
+          cy.get("#events")
+            .find(`.event:nth-of-type(${i + 1}) li:nth-of-type(2)`)
+            .invoke("text")
+            .should("contain", test_event.description);
+        });
+      });
+
+      it("Event Counts", () => {
         // TODO: there should be a better way to wait for a React re-render to update the
         // event count, but a manual wait works currently
         cy.wait(100);
@@ -104,7 +99,9 @@ describe("Admin Dashboard", () => {
           .should("not.contain", "LOADING")
           .then(function () {
             // Ensure updated event count is 1 greater then initial event count
-            expect(this.newEventCount).to.eq(this.initialEventCount + 1);
+            expect(this.newEventCount).to.eq(
+              this.initialEventCount + TEST_EVENTS.length
+            );
           });
       });
     });
