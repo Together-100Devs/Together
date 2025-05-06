54dd8afd179d61eae51dced5d4f0c0e506498ecd
ashir-valjee <ashir@eco-motive.com>
2025-05-06 21:42:38 +0100

    #516 added assertion to check for relevant text

--- cypress/e2e/404.cy.js
+++ cypress/e2e/404.cy.js
@@ -3,8 +3,11 @@
 import tgt from "../support/tgt";
 
 describe("404", () => {
-  it("Home links to home", () => {
+  it("Home links to home, and relevant text exists on the page", () => {
     cy.visit("/blablabla");
+    // Assert that the h1 contains the correct text
+    cy.get("h1").contains("Oops, looks like you are lost ...");
+
     tgt.nav.button.home().click();
     cy.url().should("eq", Cypress.config().baseUrl + "/");
   });
