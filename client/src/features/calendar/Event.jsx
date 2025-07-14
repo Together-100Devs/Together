33edf7c7e2d18f4b13d03d6bef146691bf7f9ee9
Pratik Tomar <pratiksingh067@gmail.com>
2025-07-15 00:46:55 +0530

    In mobile Title and Time of the task was not showing properly for the mobile devices | Fixed that by updating classes accoring to mobile devices

--- client/src/features/calendar/Event.jsx
+++ client/src/features/calendar/Event.jsx
@@ -12,17 +12,19 @@ const Event = (props) => {
         modal.handleOpen();
       }}
       key={props.event.title}
-      className="flex items-center shrink-0 h-5 px-1 text-sm hover:bg-gray-200"
+      className="md:flex md:items-center shrink-0 h-5 text-xs md:text-sm hover:bg-gray-200"
     >
-      <span
-        className={`shrink-0 w-2 h-2 ${
-          props.event.confirmed ? confirmedCss : unconfirmedCSss
-        } rounded-full`}
-      ></span>
-      <span className="ml-2 text-xs font-light leading-none whitespace-nowrap">
-        {formatToLocalTime(props.event.startAt)}
-      </span>
-      <span className="ml-2 font-medium leading-none truncate">
+      <div className="flex items-center shrink-0 h-5 px-1 text-xs md:text-sm hover:bg-gray-200 max-md:mb-1">
+        <span
+          className={`shrink-0 w-1.5 h-1.5 md:w-2 md:h-2 ${
+            props.event.confirmed ? confirmedCss : unconfirmedCSss
+          } rounded-full`}
+        ></span>
+        <span className="ml-1 md:ml-2 text-xs font-light md:leading-none md:whitespace-nowrap">
+          {formatToLocalTime(props.event.startAt)}
+        </span>
+      </div>
+      <span className="ml-1 md:ml-2 font-medium leading-none truncate">
         {props.event.title}
       </span>
     </button>
