c94afc10914907cfef1753ae8c07ba79d1079108
Pratik Tomar <pratiksingh067@gmail.com>
2025-07-22 07:01:33 +0530

    changed the size of font, margin , padding to adjust content for the mobile

--- client/src/features/calendar/Event.jsx
+++ client/src/features/calendar/Event.jsx
@@ -1,5 +1,5 @@
-import { formatToLocalTime } from "../../utilities/calendar";
 import { useModalContext } from "../../contexts/ModalContext";
+import { formatToLocalTime } from "../../utilities/calendar";
 
 const Event = (props) => {
   const modal = useModalContext();
@@ -12,19 +12,17 @@ const Event = (props) => {
         modal.handleOpen();
       }}
       key={props.event.title}
-      className="md:flex md:items-center shrink-0 h-5 text-xs md:text-sm hover:bg-gray-200"
+      className="flex items-center shrink-0 h-5 md:text-sm hover:bg-gray-200 cursor-pointer md:px-1"
     >
-      <div className="flex items-center shrink-0 h-5 px-1 text-xs md:text-sm hover:bg-gray-200 max-md:mb-1">
-        <span
-          className={`shrink-0 w-1.5 h-1.5 md:w-2 md:h-2 ${
-            props.event.confirmed ? confirmedCss : unconfirmedCSss
-          } rounded-full`}
-        ></span>
-        <span className="ml-1 md:ml-2 text-xs font-light md:leading-none md:whitespace-nowrap">
-          {formatToLocalTime(props.event.startAt)}
-        </span>
-      </div>
-      <span className="ml-1 md:ml-2 font-medium leading-none truncate">
+      <span
+        className={`shrink-0 w-1 h-1 md:w-2 md:h-2 ${
+          props.event.confirmed ? confirmedCss : unconfirmedCSss
+        } rounded-full`}
+      ></span>
+      <span className="ml-0.5 md:ml-2 text-[8px] md:text-sm font-light leading-none whitespace-nowrap">
+        {formatToLocalTime(props.event.startAt)}
+      </span>
+      <span className="ml-0.5 md:ml-2 text-[8px] md:text-sm font-medium leading-none truncate">
         {props.event.title}
       </span>
     </button>
