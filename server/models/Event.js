5722dacd8298639696d4f62f5752451f4a2a494f
ashir-valjee <ashir@eco-motive.com>
2025-07-29 10:47:36 +0100

    destructured constant coreectly

--- server/models/Event.js
+++ server/models/Event.js
@@ -8,7 +8,7 @@ const STRING_MAX_LENGTH = 280;
 // Event's starting date should be less than (strictly) EVENT_MAX_DATE
 const EVENT_MAX_DATE = "2030-01-01";
 // Recurring events should span no more than MAX_RECURRENCE_PERIOD number of days
-const MAX_RECURRENCE_PERIOD = require("../constants/constants");
+const { MAX_RECURRENCE_PERIOD } = require("../constants/constants");
 const DAYS_OF_WEEK = ["1", "2", "3", "4", "5", "6", "7"];
 
 const EventSchema = new mongoose.Schema(
