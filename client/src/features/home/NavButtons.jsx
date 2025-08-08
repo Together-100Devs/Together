8e2bf02e0f3edfd26fa1b8e5b7d412d203321c8e
Aswabnth.v <aswanthv695@gmail.com>
2025-08-07 19:56:12 -0700

    added cursor-pointer fully for nav Buton

--- client/src/features/home/NavButtons.jsx
+++ client/src/features/home/NavButtons.jsx
@@ -17,24 +17,24 @@ const NavButtons = ({ HomeIcon, LoginIcon, CalendarIcon }) => {
       {/* Home Button */}
       <button
         onClick={() => routing.setCurrentPage("landingPage")}
-        className="bg-white border-2 border-black rounded-2xl flex flex-col justify-center items-center p-1 w-24 h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32 focus:ring-4 focus:ring-black"
+        className="bg-white border-2 border-black rounded-2xl flex flex-col justify-center items-center p-1 w-24 h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32 focus:ring-4 focus:ring-black  cursor-pointer"
       >
         <div className="flex justify-center">
           <HomeIcon className="text-main-blue w-7 h-7 lg:w-10 lg:h-10" />
         </div>
-        <div className="text-main-blue font-black text-lg lg:text-xl xl:text-2xl cursor-pointer">
+        <div className="text-main-blue font-black text-lg lg:text-xl xl:text-2xl">
           <span>Home</span>
         </div>
       </button>
       {/* Log In & Log Out Button */}
       <button
-        className="bg-white border-2 border-black rounded-2xl flex flex-col justify-center items-center p-1 w-24 h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32 focus:ring-4 focus:ring-black"
+        className="bg-white border-2 border-black rounded-2xl flex flex-col justify-center items-center p-1 w-24 h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32 focus:ring-4 focus:ring-black  cursor-pointer"
         onClick={isAuthenticated() ? logout : handleLogin}
       >
         <div className="flex justify-center">
           <LoginIcon className="text-main-green w-7 h-7 lg:w-10 lg:h-10" />
         </div>
-        <div className="text-main-green font-black text-lg lg:text-xl xl:text-2xl cursor-pointer">
+        <div className="text-main-green font-black text-lg lg:text-xl xl:text-2xl">
           <span>{isAuthenticated() ? "Log Out" : "Log In"}</span>
         </div>
       </button>
@@ -42,12 +42,12 @@ const NavButtons = ({ HomeIcon, LoginIcon, CalendarIcon }) => {
 
       <button
         onClick={() => navigate("calendar")}
-        className="bg-white border-2 border-black rounded-2xl flex flex-col justify-center items-center p-1 w-24 h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32 focus:ring-4 focus:ring-black"
+        className="bg-white border-2 border-black rounded-2xl flex flex-col justify-center items-center p-1 w-24 h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32 focus:ring-4 focus:ring-black  cursor-pointer"
       >
         <div className="flex justify-center">
           <CalendarIcon className="text-main-orange w-7 h-7 lg:w-10 lg:h-10" />
         </div>
-        <div className="text-main-orange font-black text-lg lg:text-xl xl:text-2xl cursor-pointer">
+        <div className="text-main-orange font-black text-lg lg:text-xl xl:text-2xl">
           <span>Calendar</span>
         </div>
       </button>
